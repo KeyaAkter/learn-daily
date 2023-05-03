9fa5dc6cdf4660db39daff4b6f8733fb621be107
KeyaAkter <sanhatanvin12@gmail.com>
2023-05-03 20:23:43 +0600

    prisma added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      FontFamily: {
+        sans: ["Inter, sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
